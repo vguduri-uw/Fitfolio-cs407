66c559c703c2182c0de82410cbd167e3285e5b61
erincunningham <eecunningha2@wisc.edu>
2025-10-26 21:38:42 -0500

    Add profile image, closet title, settings button, and information button

--- app/src/main/java/com/cs407/fitfolio/ui/screens/MyClosetScreen.kt
+++ app/src/main/java/com/cs407/fitfolio/ui/screens/MyClosetScreen.kt
@@ -1,7 +1,30 @@
 package com.cs407.fitfolio.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cs407.fitfolio.R
 
 @Composable
 fun MyClosetScreen(
@@ -10,6 +33,52 @@ fun MyClosetScreen(
     onNavigateToWardrobeScreen: () -> Unit,
     onNavigateToAddScreen: () -> Unit
 ) {
-    Text("Closet screen")
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(alignment = Alignment.TopCenter)
+        ) {
+            Image(
+                // TODO: replace with actual profile image
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = "User profile image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.surfaceVariant)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "My Closet", style = MaterialTheme.typography.titleLarge)
+                IconButton(onClick = {}) { // TODO: add info onClick lambda
+                    Icon(Icons.Outlined.Info,
+                        contentDescription = "Information",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        }
+        IconButton(
+            onClick = {}, // TODO: add settings onClick lambda
+            modifier = Modifier
+                .align(alignment = Alignment.TopEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Settings,
+                contentDescription = "Settings",
+                Modifier.size(36.dp)
+            )
+        }
+    }
+    Column() {
+        Row() {
+
+        }
+    }
 }
 
