1d530f450cb5d2385cb43c5ed9ec97eed7c0cc23
Lilian4441 <llhuang@wisc.edu>
2025-10-26 22:30:35 -0500

    adding header

--- app/src/main/java/com/cs407/fitfolio/ui/screens/MyOutfitsScreen.kt
+++ app/src/main/java/com/cs407/fitfolio/ui/screens/MyOutfitsScreen.kt
@@ -1,3 +1,79 @@
 package com.cs407.fitfolio.ui.screens
 
-// test test test
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cs407.fitfolio.R
+
+@Composable
+fun MyOutfitsScreen(
+    onNavigateToClosetScreen: () -> Unit,
+    onNavigateToCalendarScreen: () -> Unit,
+    onNavigateToWardrobeScreen: () -> Unit,
+    onNavigateToAddScreen: () -> Unit
+) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(alignment = Alignment.TopCenter)
+        ) {
+            Image(
+                // TODO: replace with actual profile image
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = "User profile image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.surfaceVariant),
+                alignment = Alignment.Center
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "My Outfits", style = MaterialTheme.typography.titleLarge)
+                IconButton(onClick = {}) { // TODO: add info onClick lambda
+                    Icon(Icons.Outlined.Info,
+                        contentDescription = "Information",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        }
+        IconButton(
+            onClick = {}, // TODO: add settings onClick lambda
+            modifier = Modifier
+                .align(alignment = Alignment.TopEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Settings,
+                contentDescription = "Settings",
+                Modifier.size(36.dp)
+            )
+        }
+    }
+}
