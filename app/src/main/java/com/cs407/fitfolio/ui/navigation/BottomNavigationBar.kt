1b5059d6c419b0e0c13a76d24f5529ace2fcfd2d
Lilian4441 <llhuang@wisc.edu>
2025-10-26 22:27:42 -0500

    adding shirt icon replacement to bottom nav bar

--- app/src/main/java/com/cs407/fitfolio/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/cs407/fitfolio/ui/navigation/BottomNavigationBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -31,7 +32,9 @@ fun BottomNavigationBar(navController: NavController) {
             Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     IconButton(onClick = { navController.navigate("outfits") }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = "Outfits")
+                        Icon(
+                            painter = painterResource(id = R.drawable.shirt),
+                            contentDescription = "Outfits")
                     }
                     Text(stringResource(R.string.outfits))
                 }
