f5d2c5f897634b42733e5ac58d3e90ad15d402d0
erincunningham <eecunningha2@wisc.edu>
2025-10-27 22:08:46 -0500

    Create InformationModal.kt

---
+++ app/src/main/java/com/cs407/fitfolio/ui/modals/InformationModal.kt
@@ -0,0 +1,10 @@
+package com.cs407.fitfolio.ui.modals
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InformationModal (onDismiss: () -> Unit) {
+
+}
