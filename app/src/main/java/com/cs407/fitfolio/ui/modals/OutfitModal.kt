0f62419a9ec78e2fa662f162c2702f0418d59beb
erincunningham <eecunningha2@wisc.edu>
2025-12-02 18:01:46 -0600

    Rebasing
    # Conflicts:
    #	app/src/main/java/com/cs407/fitfolio/ui/modals/OutfitModal.kt

--- app/src/main/java/com/cs407/fitfolio/ui/modals/OutfitModal.kt
+++ app/src/main/java/com/cs407/fitfolio/ui/modals/OutfitModal.kt
@@ -29,7 +29,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -61,7 +60,6 @@ import com.cs407.fitfolio.R
 import com.cs407.fitfolio.data.ItemEntry
 import com.cs407.fitfolio.data.OutfitEntry
 import com.cs407.fitfolio.enums.DeletionStates
-import com.cs407.fitfolio.ui.screens.OutfitPreviewGrid
 import com.cs407.fitfolio.viewModels.ClosetViewModel
 import com.cs407.fitfolio.viewModels.OutfitsViewModel
 import coil.compose.rememberAsyncImagePainter
@@ -73,9 +71,6 @@ import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.rememberCoroutineScope
 import kotlinx.coroutines.launch
 import java.time.ZoneId
-import coil.compose.AsyncImage
-import androidx.compose.ui.layout.ContentScale
-
 
 // modal sheet that displays full outfit details and actions
 // shows outfit photo, description, items, and tags, with editing modes
@@ -578,7 +573,6 @@ fun outfitIconBox(
     var showDatePicker by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
 
-) {
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
