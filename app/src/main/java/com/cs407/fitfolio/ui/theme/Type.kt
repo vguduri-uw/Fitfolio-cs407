4499ad9cfe150e2f49968e07f5ecf3df39702687
Lilian4441 <llhuang@wisc.edu>
2025-12-06 13:01:58 -0600

    adding a Typography object for custom font family

--- app/src/main/java/com/cs407/fitfolio/ui/theme/Type.kt
+++ app/src/main/java/com/cs407/fitfolio/ui/theme/Type.kt
@@ -5,16 +5,47 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.cs407.fitfolio.R
+
+
+// Typography for our app (custom font)
+val FitFolioFontFamily = FontFamily(
+    Font(R.font.kudryashev_display_sans_regular, FontWeight.Normal),
+    Font(R.font.kudryashev_display_sans_regular, FontWeight.Bold)
+)
+
+val Typography = Typography(
+    displayLarge = TextStyle(fontFamily = FitFolioFontFamily),
+    displayMedium = TextStyle(fontFamily = FitFolioFontFamily),
+    displaySmall = TextStyle(fontFamily = FitFolioFontFamily),
+
+    headlineLarge = TextStyle(fontFamily = FitFolioFontFamily),
+    headlineMedium = TextStyle(fontFamily = FitFolioFontFamily),
+    headlineSmall = TextStyle(fontFamily = FitFolioFontFamily),
+
+    titleLarge = TextStyle(fontFamily = FitFolioFontFamily),
+    titleMedium = TextStyle(fontFamily = FitFolioFontFamily),
+    titleSmall = TextStyle(fontFamily = FitFolioFontFamily),
+
+    bodyLarge = TextStyle(fontFamily = FitFolioFontFamily),
+    bodyMedium = TextStyle(fontFamily = FitFolioFontFamily),
+    bodySmall = TextStyle(fontFamily = FitFolioFontFamily),
+
+    labelLarge = TextStyle(fontFamily = FitFolioFontFamily),
+    labelMedium = TextStyle(fontFamily = FitFolioFontFamily),
+    labelSmall = TextStyle(fontFamily = FitFolioFontFamily),
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +62,4 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
+//)
